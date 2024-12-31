607ca0538b0baa1f5f228791580edfc8e300a285
Farman71 <farmanhussain71@gmail.com>
2024-12-31 16:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let input1 = prompt("Enter the first value:");
 let input2 = prompt("Enter the second value:");
 
 // Convert inputs intelligently
-let value1 = isNaN(Number(input1)) ? input1 : Number(input1);
-let value2 = isNaN(Number(input2)) ? input2 : Number(input2);
+let value1 = !isNaN(input1) && input1.trim() != "" ? Number(input1) : input1;
+let value2 = !isNaN(input2) && input2.trim() != "" ? Number(input2) : input2;
 
 alert(isSameType(value1, value2).toString());
